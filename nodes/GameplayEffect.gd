59755982b1eef6ac8a20c528d41c4b99d96a6e0e
octod <iamoctod@gmail.com>
2021-02-03 17:33:48 +0100

    chore: renames internals

--- nodes/GameplayEffect.gd
+++ nodes/GameplayEffect.gd
@@ -17,7 +17,7 @@ signal effect_deactivated
 
 func _ready():
 	if not Engine.editor_hint:
-		setup_effect()
+		_setup_effect()
 		if should_activate(EffectActivationEvent.ImmediateActivation):
 			apply_effect()
 
@@ -32,26 +32,26 @@ func play_effect() -> void:
 func pause_effect() -> void:
 	pass
 
-func setup_effect() -> void:
-	connect_to_parent_signal()
+func _setup_effect() -> void:
+	_connect_to_parent_signal()
 
 
 func apply_effect() -> void:
 	pass
 
 
-func connect_to_parent_signal() -> void:
+func _connect_to_parent_signal() -> void:
 	var parent = get_parent()
 
 	if parent.has_signal("attribute_changed"):
-		parent.connect("attribute_changed", self, "on_attribute_changed")
+		parent.connect("attribute_changed", self, "_on_attribute_changed")
 
 
 func get_parent_attribute_map():
 	return get_parent()
 
 
-func on_attribute_changed(attribute: GameplayAttribute) -> void:
+func _on_attribute_changed(attribute: GameplayAttribute) -> void:
 	if should_activate(EffectActivationEvent.AttributeChanged):
 		apply_effect();
 
