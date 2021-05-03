080ceb66cdb97bc42b8fcfde4fc1952dba8ec706
octod <iamoctod@gmail.com>
2021-05-03 09:05:58 +0200

    chore: changes paths

--- addons/godot-gameplay-attributes/gameplay_attributes.gd
+++ addons/godot-gameplay-attributes/gameplay_attributes.gd
@@ -3,22 +3,22 @@ extends EditorPlugin
 
 
 func _enter_tree():
-	var attribute_node_icon = load("res://addons/gameplay_attributes/assets/attribute_icon.png");
-	var gameplay_effect_icon = load("res://addons/gameplay_attributes/assets/timed_icon.png")
-	var damage_effect_icon = load("res://addons/gameplay_attributes/assets/damage_gameplay_effect.png")
-	var restore_effect_icon = load("res://addons/gameplay_attributes/assets/restore_attribute_icon.png")
+	var attribute_node_icon = load("res://addons/godot-gameplay-attributes/assets/attribute_icon.png");
+	var gameplay_effect_icon = load("res://addons/godot-gameplay-attributes/assets/timed_icon.png")
+	var damage_effect_icon = load("res://addons/godot-gameplay-attributes/assets/damage_gameplay_effect.png")
+	var restore_effect_icon = load("res://addons/godot-gameplay-attributes/assets/restore_attribute_icon.png")
 	
 	# basenodes
-	add_custom_type("GameplayAttributeMap", "Node", load("res://addons/gameplay_attributes/nodes/GameplayAttributeMap.gd"), attribute_node_icon);
-	add_custom_type("GameplayAttribute", "Node", load("res://addons/gameplay_attributes/nodes/GameplayAttribute.gd"), attribute_node_icon);
-	add_custom_type("GameplayEffect", "Node", load("res://addons/gameplay_attributes/nodes/GameplayEffect.gd"), gameplay_effect_icon);
-	add_custom_type("TimedGameplayEffect", "GameplayEffect", load("res://addons/gameplay_attributes/nodes/TimedGameplayEffect.gd"), gameplay_effect_icon);
+	add_custom_type("GameplayAttributeMap", "Node", load("res://addons/godot-gameplay-attributes/nodes/GameplayAttributeMap.gd"), attribute_node_icon);
+	add_custom_type("GameplayAttribute", "Node", load("res://addons/godot-gameplay-attributes/nodes/GameplayAttribute.gd"), attribute_node_icon);
+	add_custom_type("GameplayEffect", "Node", load("res://addons/godot-gameplay-attributes/nodes/GameplayEffect.gd"), gameplay_effect_icon);
+	add_custom_type("TimedGameplayEffect", "GameplayEffect", load("res://addons/godot-gameplay-attributes/nodes/TimedGameplayEffect.gd"), gameplay_effect_icon);
 
 	# premade gameplay effects
-	add_custom_type("AttributeConsumeGameplayEffect", "TimedGameplayEffect", load("res://addons/gameplay_attributes/premade_gameplay_effects/AttributeConsumeGameplayEffect.gd"), gameplay_effect_icon);
-	add_custom_type("AttributeRegenGameplayEffect", "TimedGameplayEffect", load("res://addons/gameplay_attributes/premade_gameplay_effects/AttributeRegenGameplayEffect.gd"), gameplay_effect_icon);
-	add_custom_type("DamageGameplayEffect", "GameplayEffect", load("res://addons/gameplay_attributes/premade_gameplay_effects/DamageGameplayEffect.gd"), damage_effect_icon);
-	add_custom_type("RestoreGameplayEffect", "GameplayEffect", load("res://addons/gameplay_attributes/premade_gameplay_effects/RestoreGameplayEffect.gd"), restore_effect_icon);
+	add_custom_type("AttributeConsumeGameplayEffect", "TimedGameplayEffect", load("res://addons/godot-gameplay-attributes/premade_gameplay_effects/AttributeConsumeGameplayEffect.gd"), gameplay_effect_icon);
+	add_custom_type("AttributeRegenGameplayEffect", "TimedGameplayEffect", load("res://addons/godot-gameplay-attributes/premade_gameplay_effects/AttributeRegenGameplayEffect.gd"), gameplay_effect_icon);
+	add_custom_type("DamageGameplayEffect", "GameplayEffect", load("res://addons/godot-gameplay-attributes/premade_gameplay_effects/DamageGameplayEffect.gd"), damage_effect_icon);
+	add_custom_type("RestoreGameplayEffect", "GameplayEffect", load("res://addons/godot-gameplay-attributes/premade_gameplay_effects/RestoreGameplayEffect.gd"), restore_effect_icon);
 		
 
 func _exit_tree():
