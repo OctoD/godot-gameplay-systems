e16be25a174aad90096c1c78e9da6cfaaa91e58f
octod <iamoctod@gmail.com>
2023-01-30 15:29:20 +0100

    feat: adds effected_area2d/3d nodes

---
+++ addons/godot-gameplay-attributes/nodes/effected_area2d.gd
@@ -0,0 +1,37 @@
+@tool
+class_name EffectedArea2D extends Area2D
+
+
+@export_category("Gameplay Effect Handling")
+@export var remove_self_on_apply := true
+@export var remove_effects_on_apply := true
+
+
+var effects: Array[GameplayEffect] = []:
+	get:
+		var _effects: Array[GameplayEffect] = []
+		
+		for child in get_children():
+			if child is GameplayEffect:
+				_effects.append(child)
+		
+		return _effects
+
+
+func _ready() -> void:
+	if not Engine.is_editor_hint():
+		body_entered.connect(func (body: Node2D):
+			if should_apply_effect(body):
+				for effect in effects:
+					if remove_effects_on_apply:
+						remove_child(effect)
+						body.add_child(effect)
+					else:
+						body.add_child(effect.duplicate())
+				if remove_self_on_apply:
+					queue_free()
+		)
+
+
+func should_apply_effect(node: Node2D) -> bool:
+	return true
--- addons/godot-gameplay-attributes/plugin.gd
+++ addons/godot-gameplay-attributes/plugin.gd
@@ -7,10 +7,14 @@ const ATTRIBUTE_TABLE_RESOURCE_NAME = "AttributeTableResource"
 const ATTRIBUTE_EFFECT_RESOURCE_NAME = "AttributeEffectResource"
 const GAMEPLAY_EFFECT_NAME = "GameplayEffect"
 const GAMEPLAY_ATTRIBUTE_MAP_NAME = "GameplayAttributeMap"
+const EFFECTED_AREA2D = "EffectedArea2D"
+const EFFECTED_AREA3D = "EffectedArea3D"
 
 var attributes_table_resource = preload("res://addons/godot-gameplay-attributes/resources/attribute_table.gd")
 var attribute_resource = preload("res://addons/godot-gameplay-attributes/resources/attribute.gd")
 var attribute_effect_resource = preload("res://addons/godot-gameplay-attributes/resources/attribute_effect.gd")
+var effected_area2d = preload("res://addons/godot-gameplay-attributes/nodes/effected_area2d.gd")
+var effected_area3d = preload("res://addons/godot-gameplay-attributes/nodes/effected_area3d.gd")
 var gameplay_effect = preload("res://addons/godot-gameplay-attributes/nodes/gameplay_effect.gd")
 var gameplay_attribute_map = preload("res://addons/godot-gameplay-attributes/nodes/gameplay_attribute_map.gd")
 
@@ -27,6 +31,8 @@ func _enter_tree():
 	add_custom_type(ATTRIBUTE_EFFECT_RESOURCE_NAME, "Resource", attribute_effect_resource, preload("res://addons/godot-gameplay-attributes/assets/effect_icon.png"))
 	add_custom_type(GAMEPLAY_ATTRIBUTE_MAP_NAME, "Node", gameplay_attribute_map, preload("res://addons/godot-gameplay-attributes/assets/attribute_map_icon.png"))
 	add_custom_type(GAMEPLAY_EFFECT_NAME, "Node", gameplay_effect, preload("res://addons/godot-gameplay-attributes/assets/effect_icon.png"))
+	add_custom_type(EFFECTED_AREA2D, "Area2D", effected_area2d, null)
+	add_custom_type(EFFECTED_AREA3D, "Area3D", effected_area3d, null)
 
 	attribute_inspector_plugin = attribute_inspector_plugin_script.new()
 	effect_inspector_plugin = effect_inspector_plugin_script.new()
@@ -41,6 +47,8 @@ func _exit_tree():
 	remove_custom_type(ATTRIBUTE_EFFECT_RESOURCE_NAME)
 	remove_custom_type(GAMEPLAY_ATTRIBUTE_MAP_NAME)
 	remove_custom_type(GAMEPLAY_EFFECT_NAME)
+	remove_custom_type(EFFECTED_AREA2D)
+	remove_custom_type(EFFECTED_AREA3D)
 	
 	remove_inspector_plugin(attribute_inspector_plugin)
 	remove_inspector_plugin(effect_inspector_plugin)
