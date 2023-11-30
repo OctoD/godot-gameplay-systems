af3373d0529fdb0cfef3a1244fe5f7940ad08d2a
Daniel Bernard <daniel.john.bernard@gmail.com>
2023-11-30 02:07:45 -0600

    changes load to preload to support 4.2 relocatability change (#46)
    fix: fixes issue #45

--- addons/godot_gameplay_systems/camera_shake/plugin.gd
+++ addons/godot_gameplay_systems/camera_shake/plugin.gd
@@ -3,7 +3,7 @@ extends EditorPlugin
 
 
 func _enter_tree() -> void:
-	add_custom_type("CameraShake", "Node", load("./nodes/camera_shake.gd"), null)
+	add_custom_type("CameraShake", "Node", preload("./nodes/camera_shake.gd"), null)
 
 
 func _exit_tree() -> void:
--- addons/godot_gameplay_systems/interactables/plugin.gd
+++ addons/godot_gameplay_systems/interactables/plugin.gd
@@ -2,12 +2,12 @@ extends EditorPlugin
 
 
 func _enter_tree() -> void:
-	add_custom_type("InteractableArea2D", "Area2D", load("./nodes/2d/interactable_area_2d.gd"), load("./assets/InteractableArea2D.png"))
-	add_custom_type("InteractableArea3D", "Area3D", load("./nodes/3d/interactable_area_3d.gd"), load("./assets/InteractableArea2D.png"))
-	add_custom_type("InteractionRayCast2D", "RayCast2D", load("./nodes/2d/interaction_raycast_2d.gd"), load("./assets/InteractionRayCast2DIcon.png"))
-	add_custom_type("InteractionRayCast3D", "RayCast3D", load("./nodes/3d/interaction_raycast_3d.gd"), load("./assets/InteractionRayCast3DIcon.png"))
-	add_custom_type("InteractionManager", "Node", load("./nodes/interaction_manager.gd"), load("./assets/InteractionIcon.png"))
-	add_custom_type("Interaction", "Resource", load("./resources/interaction.gd"), load("./assets/InteractionIcon.png"))
+	add_custom_type("InteractableArea2D", "Area2D",preload("./nodes/2d/interactable_area_2d.gd"),preload("./assets/InteractableArea2D.png"))
+	add_custom_type("InteractableArea3D", "Area3D",preload("./nodes/3d/interactable_area_3d.gd"),preload("./assets/InteractableArea2D.png"))
+	add_custom_type("InteractionRayCast2D", "RayCast2D",preload("./nodes/2d/interaction_raycast_2d.gd"),preload("./assets/InteractionRayCast2DIcon.png"))
+	add_custom_type("InteractionRayCast3D", "RayCast3D",preload("./nodes/3d/interaction_raycast_3d.gd"),preload("./assets/InteractionRayCast3DIcon.png"))
+	add_custom_type("InteractionManager", "Node",preload("./nodes/interaction_manager.gd"),preload("./assets/InteractionIcon.png"))
+	add_custom_type("Interaction", "Resource",preload("./resources/interaction.gd"),preload("./assets/InteractionIcon.png"))
 
 
 func _exit_tree() -> void:
